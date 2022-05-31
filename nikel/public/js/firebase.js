3ad5d2108b8c9ec6175688b1f7c17d2bf6cafd80
Suh <suellen.org@gmail.com>
2022-05-31 18:18:37 -0300

    para teste

---
+++ nikel/public/js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZjUq4N24vPM-rQ-rPvOQJPLpsSIJZ5VQ",
+    authDomain: "trabricardo-dc4ee.firebaseapp.com",
+    projectId: "trabricardo-dc4ee",
+    storageBucket: "trabricardo-dc4ee.appspot.com",
+    messagingSenderId: "404028404881",
+    appId: "1:404028404881:web:9aed0b282b24d9edb77b30",
+    measurementId: "G-C4FL313Z9B",
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ nikel/public/js/index.js
@@ -0,0 +1,91 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    getAccount(email).then(account => {
+        if (!account) {
+            alert("Opps! Verifique o usuário ou a senha.");
+            return;
+        }
+
+        if (account) {
+            if (account.password !== password) {
+                alert("Opps! Verifique o usuário ou a senha.");
+                return;
+            }
+
+            login(email, checkSession);
+        }
+    });
+
+
+});
+
+//CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if (password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 dígitos.");
+        return;
+    }
+
+    firebase.firestore().collection("accounts").add({
+        email,
+        password,
+    }).then(x => {
+        alert("Conta criada com sucesso!");
+        myModal.hide();
+    });
+});
+
+function checkLogged() {
+    if (session) {
+        session.setItem("logged", session);
+        logged = session;
+    }
+
+    if (logged) {
+        login(logged, session);
+    }
+}
+
+function login(email, session){
+    saveSession(email, session);
+    window.location.href = "./home.html";
+}
+
+function saveSession(data, saveSession) {
+    if (saveSession) {
+        localStorage.setItem("session", data);
+    }
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    return firebase.firestore().collection("accounts").where("email", "==", key).get().then(function (doc) {
+        if (doc.size == 1) {
+            console.log("Document data:", doc.docs[0].data());
+            return doc.docs[0].data();
+        } else {
+            return null;
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+}
